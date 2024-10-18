7a0ddcc4f4b50e3473f650f2fb2ed976fe424538
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-10-18 17:45:39 -0300

    creating the presentation and description

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
-import ContactMe from "./Components/ContactMe";
+// import ContactMe from "./Components/ContactMe";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home/>}/>
         <Route path="/contactMe" />
         <Route path="/projects" />
       </Routes>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,12 @@
-
+import styles from './Home.module.css'
 
 function Home() {
   return (
-    <h1>Home content</h1>
+    <div className={styles.presentation}>
+      <h1 className={styles.myNameIs}>Hi, my name is <span className={styles.name}>Kaynan.</span></h1>
+      <h1 className={styles.whatAmI}>I'm a Front-End Developer.</h1>
+      <h3 className={styles.description}>I'm a Front-End Developer with experience in <span className={styles.name}>JavaScript, React, HTML, CSS,</span> and tools like <span className={styles.name}>Git</span> and <span className={styles.name}>Cypress.</span> I've built dynamic, user-friendly applications like the Aluguel Marina Da Glória website. I'm eager to tackle new challenges and grow in the tech industry.</h3>
+    </div>
   )
 }
 
