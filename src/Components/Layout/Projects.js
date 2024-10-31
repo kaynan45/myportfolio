69ea34a51af3cea0700f66e609ee30637e0bb0a4
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-10-31 09:25:28 -0300

    started the projects cards

--- src/Components/Layout/Projects.js
+++ src/Components/Layout/Projects.js
@@ -1,4 +1,5 @@
-import styles from './Projects.module.css'
+import ProjectCards from "../Utils/ProjectCards";
+import styles from "./Projects.module.css";
 
 function Projects() {
   return (
@@ -8,8 +9,20 @@ function Projects() {
         <p className={styles.aboutTitle}>Projects</p>
         <hr className={styles.aboutDivider} aria-orientation="horizontal" />
       </div>
+      <ProjectCards
+        projectName="Book Tracker"
+        projectImage="https://picsum.photos/600/300"
+        projectLink="/"
+        projectDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries."
+      />
+      <ProjectCards
+        projectName="Weather App"
+        projectImage="https://picsum.photos/600/300"
+        projectLink="/"
+        projectDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries."
+      />
     </section>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
---
+++ src/Components/Utils/ProjectCards.js
@@ -0,0 +1,22 @@
+import styles from "./ProjectCards.module.css";
+
+//{ projectName }, { projectImage }, { projectLink }
+function ProjectCards({
+  projectImage,
+  projectName,
+  projectLink,
+  projectDescription,
+}) {
+  return (
+    <div className={styles.projectCard}>
+      <img src={projectImage} alt="projectImage"></img>
+      <div className={styles.writingArea}>
+        <h1>{projectName}</h1>
+        <p>{projectDescription}</p>
+        <button className={styles.projectButton}>Project Link</button>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCards;
