8e7e080283b5be92b4a3ca29797f1a00663a1849
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-11-07 10:02:18 -0300

    added the description of the projects

--- src/Components/Layout/Projects.js
+++ src/Components/Layout/Projects.js
@@ -13,13 +13,13 @@ function Projects() {
         projectName="Book Tracker"
         projectImage="/img/book-tracker-website.png"
         projectLink="https://portifolio-orcin-ten.vercel.app/"
-        projectDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries."
+        projectDescription="I developed this book tracker project to solve my personal challenge of managing books I've read and those I wish to buy. The app allows users to add books to a 'Book List' for easy tracking, providing a streamlined experience that helps organize reading goals and potential purchases. Each added book includes a link to Amazon, directing users to relevant offers and making purchasing convenient. This project demonstrates my ability to design and implement functional, user-centric applications that address real-world needs, showcasing my skills in front-end development and integration with external platforms."
       />
       <ProjectCards
         projectName="Weather App"
         projectImage="/img/weather-app-website.png"
         projectLink="https://weather-app-khaki-five-38.vercel.app/"
-        projectDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries."
+        projectDescription="I built this weather app to quickly and conveniently check the weather in any location worldwide. The app allows users to enter any city or location and receive current weather conditions, along with forecasts, helping them plan their day with ease. Through this project, I used my skills in integrating APIs and creating responsive, user-friendly interfaces, showcasing my ability to develop practical applications that solve everyday needs."
       />
     </section>
   );
